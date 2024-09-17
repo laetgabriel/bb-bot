c9f4222f1b9fb9bc63cc663e289db41bb8626767
Gabriel Laet <gabriellaetfm12@gmail.com>
2024-09-17 14:20:30 -0300

    add Config bot

--- src/main/java/org/laetproject/Bot.java
+++ src/main/java/org/laetproject/Bot.java
@@ -1,5 +1,6 @@
 package org.laetproject;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
@@ -8,12 +9,19 @@ import javax.security.auth.login.LoginException;
 
 public class Bot {
 
+    private final Dotenv config;
+
     public Bot() throws LoginException {
-        String token = System.getenv("TOKEN");
+        config = Dotenv.configure().ignoreIfMissing().load();
+        String token = config.get("TOKEN");
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         builder.setStatus(OnlineStatus.DO_NOT_DISTURB)
                 .setActivity(Activity.listening("Mamonas Assassinas")).build();
 
+    }
+
+    public Dotenv getConfig() {
+        return config;
     }
 
 }
--- src/main/java/org/laetproject/Main.java
+++ src/main/java/org/laetproject/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(System.getenv("TOKEN"));
         try{
             Bot bot = new Bot();
         } catch (LoginException e) {
